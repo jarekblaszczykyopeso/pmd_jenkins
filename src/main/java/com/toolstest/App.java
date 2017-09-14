8f5f62136a4d46e15fb05efa0f005fc5e2195f21
jarekblaszczykyopeso <jaroslaw.blaszczyk@yopeso.com>
2017-09-14 11:51:35 +0300

    normal

--- src/main/java/com/toolstest/App.java
+++ src/main/java/com/toolstest/App.java
@@ -4,7 +4,7 @@ package com.toolstest;
  * Hello world!
  */
 public class App {
-    int inutile = 5;
+    private int inutile = 5;
 
     public static void main(String[] args) {
         System.out.println("Hello World!");
